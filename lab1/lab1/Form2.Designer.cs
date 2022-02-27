c67aa08907b989d22794a93787ee8031cbe614ba
kathy_kaffeine <eeprimakova@stud.etu.ru>
2022-02-27 15:46:53 +0300

    1 task, to be checked

---
+++ lab1/lab1/Form2.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace lab1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "вращать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(663, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(663, 343);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(649, 438);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1248, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "В первую ячейку необходимо ввести угол отклонения центра круга от точки, координа" +
+    "ты которой необходимо ввести во вторую и третью ячейки, после чего нажать на кно" +
+    "пку.";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(663, 310);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ lab1/lab1/Form2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form2 : Form
+    {
+        static int size = 100;
+        static Size diameter = new Size(size * 2, size * 2);
+        Pen pen = new Pen(Color.Magenta, 10);
+        Color background = Color.AntiqueWhite;
+        bool f = false;
+        static Point point = new Point(0, 0);
+        Rectangle rect = new Rectangle(point, diameter);
+
+        static bool isNumber(String s)
+        {
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] != '0') || (s[i] != '1') || (s[i] != '2') || (s[i] != '3') || (s[i] != '4') ||
+                    (s[i] != '5') || (s[i] != '6') || (s[i] != '7') || (s[i] != '8') || (s[i] != '9'))
+                    return false;
+            }
+            return true;
+        }
+
+        static bool isDouble(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] != '0') || (s[i] != '1') || (s[i] != '2') || (s[i] != '3') || (s[i] != '4') ||
+                    (s[i] != '5') || (s[i] != '6') || (s[i] != '7') || (s[i] != '8') || (s[i] != '9') 
+                    || (s[i] != '.') || (s[i] != ','))
+                    return false;
+            }
+            return true;
+        }
+
+        static Point RotatePoint(Point pointToRotate, Point centerPoint, double angleInDegrees)
+        { // По часовой +, против часовой -
+            double angleInRadians = angleInDegrees * (Math.PI / 180);
+            double cosTheta = Math.Cos(angleInRadians);
+            double sinTheta = Math.Sin(angleInRadians);
+            return new Point
+            {
+                X =
+                    (int)
+                    (cosTheta * (pointToRotate.X - centerPoint.X) -
+                    sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X),
+                Y =
+                    (int)
+                    (sinTheta * (pointToRotate.X - centerPoint.X) +
+                    cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y)
+            };
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "Кручу-верчу";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f = true;
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(BackColor);
+            pictureBox1.Refresh();
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            if (f)
+            {
+                String x = textBox2.Text.Trim();
+                String y = textBox1.Text;
+                String a = textBox3.Text;
+                int ix, iy; double ia;
+                if (int.TryParse(x, out ix) && int.TryParse(y, out iy) && double.TryParse(a, out ia))
+                {
+                    Point point1 = new Point(Convert.ToInt32(x), Convert.ToInt32(y));
+                    point = RotatePoint(point, point1, Convert.ToDouble(a));
+                    rect = new Rectangle(point, diameter);
+                    e.Graphics.DrawEllipse(pen, rect);
+                    rect = new Rectangle(point1, new Size(10, 10));
+                    e.Graphics.DrawEllipse(pen, rect);
+                }
+            }
+            else
+            {
+                e.Graphics.DrawEllipse(pen, rect);
+            }
+        }
+    }
+}
