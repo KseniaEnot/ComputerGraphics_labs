e2ef33b0fb385ee6a4965849656b5910c8214707
kathy_kaffeine <eeprimakova@stud.etu.ru>
2022-03-20 14:57:47 +0300

    e lost

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1 = new System.Windows.Forms.NumericUpDown();
+            this.y1 = new System.Windows.Forms.NumericUpDown();
+            this.y2 = new System.Windows.Forms.NumericUpDown();
+            this.x2 = new System.Windows.Forms.NumericUpDown();
+            this.y4 = new System.Windows.Forms.NumericUpDown();
+            this.x4 = new System.Windows.Forms.NumericUpDown();
+            this.y3 = new System.Windows.Forms.NumericUpDown();
+            this.x3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.power = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.x1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.power)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(628, 33);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(77, 22);
+            this.x1.TabIndex = 0;
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(711, 33);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(77, 22);
+            this.y1.TabIndex = 1;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(711, 61);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(77, 22);
+            this.y2.TabIndex = 3;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(628, 61);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(77, 22);
+            this.x2.TabIndex = 2;
+            // 
+            // y4
+            // 
+            this.y4.Location = new System.Drawing.Point(711, 117);
+            this.y4.Name = "y4";
+            this.y4.Size = new System.Drawing.Size(77, 22);
+            this.y4.TabIndex = 7;
+            // 
+            // x4
+            // 
+            this.x4.Location = new System.Drawing.Point(628, 117);
+            this.x4.Name = "x4";
+            this.x4.Size = new System.Drawing.Size(77, 22);
+            this.x4.TabIndex = 6;
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(711, 89);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(77, 22);
+            this.y3.TabIndex = 5;
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(628, 89);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(77, 22);
+            this.x3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "координаты x и y";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(609, 431);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(682, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "степень";
+            // 
+            // power
+            // 
+            this.power.Location = new System.Drawing.Point(677, 162);
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(77, 22);
+            this.power.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(624, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 31);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "рисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y4);
+            this.Controls.Add(this.x4);
+            this.Controls.Add(this.y3);
+            this.Controls.Add(this.x3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.x1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.power)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown x1;
+        private System.Windows.Forms.NumericUpDown y1;
+        private System.Windows.Forms.NumericUpDown y2;
+        private System.Windows.Forms.NumericUpDown x2;
+        private System.Windows.Forms.NumericUpDown y4;
+        private System.Windows.Forms.NumericUpDown x4;
+        private System.Windows.Forms.NumericUpDown y3;
+        private System.Windows.Forms.NumericUpDown x3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown power;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab2/lab2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        Pen pen = new Pen(Color.Magenta, 3);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            List<PointF> input_points = new List<PointF>(); // введённые точки
+            input_points.Add(new PointF((float)x1.Value, (float)y1.Value));
+            input_points.Add(new PointF((float)x2.Value, (float)y2.Value));
+            input_points.Add(new PointF((float)x3.Value, (float)y3.Value));
+            input_points.Add(new PointF((float)x4.Value, (float)y4.Value));
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(BackColor);
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(BackColor);
+        }
+    }
+
+    class BSplineCurve
+    {
+        int Degree; //порядок сплайна
+        int Resolution; //количество точек сплайна
+        List<float> Knots = new List<float>(); //узлы сплайна
+        List<PointF> ControlPoints = new List<PointF>();
+
+        //Получить текущий интервал
+        private int getInterval(float currentPosition)
+        {
+            for (int whichInterval = 0; whichInterval < Knots.Count - 1; whichInterval++)
+            {
+                if (Knots[whichInterval] <= currentPosition && currentPosition < Knots[whichInterval + 1]) return whichInterval;
+            }
+            return 0;
+        }
+        //Расчет текущей точки
+        private void calcCurrentPoint(float currentPosition, int whichInterval, ref PointF pnt)
+        {
+            List<PointF> A = new List<PointF>();
+            int i, j, r;
+            float d1, d2;
+
+            for (j = 0; j < Degree; j++)
+                A.Add(ControlPoints[whichInterval - Degree + 1 + j]);
+
+            for (r = 1; r < Degree; r++)
+            {
+                for (j = Degree - 1; j >= r; j--)
+                {
+                    i = whichInterval - Degree + 1 + j;
+                    d1 = (currentPosition - Knots[i]); d2 = (Knots[i + Degree - r] - currentPosition);
+                    PointF newPoint = new PointF((d1 * A[j].X + d2 * A[j - 1].X) / (d1 + d2), (d1 * A[j].Y + d2 * A[j - 1].Y) / (d1 + d2));
+                    A[j] = newPoint;
+                }
+            }
+            pnt.X = A[Degree - 1].X;
+            pnt.Y = A[Degree - 1].Y;
+        }
+
+        public List<PointF> points = new List<PointF>(); // точки для кривой
+
+        /*
+        * t - порядок сплайна (степень кривой),
+        * Control - контрольные точки,
+        * knots - узлы сплайна
+        */
+        public BSplineCurve(int t, ref List<PointF> Control, ref List<float> knots)
+        {
+            Degree = t; //степень сплайна
+            Resolution = 200;
+
+            for (int i = 0; i < Control.Count; i++) ControlPoints.Add(Control[i]);
+
+            for (int i = 0; i < knots.Count; i++) Knots.Add(knots[i]);
+
+            float currentPosition = 0;
+            float step = (float)(1.0 / Resolution);
+            PointF PNT = new PointF();
+
+            for (int j = 0; j < Resolution; j++)
+            {
+                int whichInterval = getInterval(currentPosition);
+
+                calcCurrentPoint(currentPosition, whichInterval, ref PNT);
+
+                points.Add(PNT);
+                currentPosition += step;
+            }
+        }
+    }
+}
